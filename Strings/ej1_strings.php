c760f6eda4fd9c2804260d9ce94a05840b56e5a7
Óscar Bernases de la Torre <oscarbernases02@gmail.com>
2026-09-21 13:25:14 +0200

    Resuelto EJ1

--- Strings/ej1_strings.php
+++ Strings/ej1_strings.php
@@ -3,6 +3,18 @@
 <BODY>
 <?php
     $ip="192.18.16.204";
+    $ip2="10.33.161.2";
+
+    $partes = explode(".", $ip);
+
+    $binario = sprintf("%08b.%08b.%08b.%08b", $partes[0],$partes[1],$partes[2],$partes[3]);
+    $partes = explode(".", $ip2);
+    $binario2 = sprintf("%08b.%08b.%08b.%08b", $partes[0],$partes[1],$partes[2],$partes[3]);
+
+    echo "IP {$ip} en binario es {$binario}<br>";
+    echo "IP {$ip2} en binario es {$binario2}";
+
+    
 ?>
 </BODY>
 </HTML>
