b0161915aee3c853c97138188ece02e0335f693d
Óscar Bernases de la Torre <oscarbernases02@gmail.com>
2026-09-21 13:36:33 +0200

    Resuelto EJ2

--- Strings/ej2_strings.php
+++ Strings/ej2_strings.php
@@ -5,10 +5,20 @@
     $nombre = " aLBeRTo gaRCia loPEz ";
 
     $nombreLimpio = trim($nombre);
-    echo "Cadena original: {$nombreLimpio}<br>";
-
     $nombreNormalizado = ucwords(strtolower($nombreLimpio));
+    $caracteres = strlen($nombreLimpio);
+    $partes = explode(" ", $nombreNormalizado);
+    $iniciales = substr($partes[0], 0, 1) . "." . substr($partes[1], 0, 1) . "." . substr($partes[2], 0, 1);
+    $nombreUsuario = strtolower($partes[0]) . "." . strtolower($partes[1]);
+
+    echo "Cadena original: {$nombre}<br>";
     echo "Nombre normalizado: {$nombreNormalizado}<br>";
+    echo "Número de caracteres: {$caracteres}<br>";
+    echo "Nombre: {$partes[0]}<br>";
+    echo "Primer apellido: {$partes[1]}<br>";
+    echo "Segundo apellido: {$partes[2]}<br>";
+    echo "Iniciales: {$iniciales}<br>";
+    echo "Nombre de usuario: {$nombreUsuario}";
 ?>
 </BODY>
 </HTML>
