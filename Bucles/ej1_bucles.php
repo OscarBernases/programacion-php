f1204b4a02b0c43a13b63896521eb90a7d35a9d0
Óscar Bernases de la Torre <oscarbernases02@gmail.com>
2026-09-22 13:20:58 +0200

    Añadido EJ1

---
+++ Bucles/ej1_bucles.php
@@ -0,0 +1,37 @@
+<HTML>
+<HEAD><TITLE> EJ1 Bucles – Estadística secuencia </TITLE></HEAD>
+<BODY>
+<?php
+    $inicio = 1;
+    $fin = 100;
+
+    
+    $pares = 0;
+    $impares = 0;
+    $multiplosTres = 0;
+    $suma = 0;
+
+    for ($i = $inicio; $i <= $fin; $i++) {
+
+        $suma = $suma + $i;
+
+        if ($i % 2 == 0) {
+            $pares++;
+        } else {
+            $impares++;
+        }
+
+        if ($i % 3 == 0) {
+            $multiplosTres++;
+        }
+    }
+
+    echo "Números del {$inicio} al {$fin}<br><br>";
+    echo "Cantidad de números: {$i}<br>";
+    echo "Números pares: {$pares}<br>";
+    echo "Números impares: {$impares}<br>";
+    echo "Multiplos de 3: {$multiplosTres}<br>";
+    echo "Suma total: {$suma}";
+?>
+</BODY>
+</HTML>
